05182a6d8879928bd96bee5b8920d4d939da706d
Maria Garcia <mabeg920@hotmail.com>
2025-06-05 16:53:32 -0500

    comunicacion REST establecida

--- microservicio_usuarios/app.py
+++ microservicio_usuarios/app.py
@@ -1,1 +1,30 @@
-print("hello world")
+from flask import Flask, request, jsonify
+from werkzeug.security import generate_password_hash
+import os
+
+app = Flask(__name__)
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    username = data.get('usuario')
+    email = data.get('email')
+    password = data.get('contrasena')
+
+    if not username or not email or not password:
+        return jsonify({"error": "Faltan datos obligatorios"}), 400
+    
+    hashed_password = generate_password_hash(password)
+
+    return jsonify({
+        "mensaje": "Usuario recibido correctamente",
+        "data":{
+            "usuario": username,
+            "email": email,
+            "contrasena_encriptada": hashed_password
+        }
+    }), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
