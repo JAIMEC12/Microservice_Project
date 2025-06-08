e94748b9c803835174ae830e6ad0e13bbbc9dd19
JAIME_CARRIEL <jaimecarriel302@gmail.com>
2025-06-07 21:56:19 -0500

    Kafka_Communication

--- microservicio_usuarios/app.py
+++ microservicio_usuarios/app.py
@@ -1,8 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
 from werkzeug.security import generate_password_hash
-import os
+import os, kafka,time
+from kafka.errors import NoBrokersAvailable
 
 app = Flask(__name__)
+
+producer = kafka.KafkaProducer(
+    bootstrap_servers=['kafka:9092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+    api_version=(2, 0, 2)
+)
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -17,9 +24,12 @@ def register():
     
     hashed_password = generate_password_hash(password)
 
+    producer.send('usuarios', data)
+    producer.flush()
+
     return jsonify({
         "mensaje": "Usuario recibido correctamente",
-        "data":{
+        "data": {
             "usuario": username,
             "email": email,
             "contrasena_encriptada": hashed_password
@@ -27,4 +37,5 @@ def register():
     }), 201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    
+    app.run(host='0.0.0.0', port=5000, debug=True)
