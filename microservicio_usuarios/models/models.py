283f10c547faddb2e5a9fe6dd5f9a547b934ff1d
JAIME_CARRIEL <jaimecarriel302@gmail.com>
2025-06-10 01:04:17 -0500

    docker-compose and models.py comments added

--- microservicio_usuarios/models/models.py
+++ microservicio_usuarios/models/models.py
@@ -1,14 +1,16 @@
-from flask_sqlalchemy import SQLAlchemy
+#DEPENDENCIAS
+from flask_sqlalchemy import SQLAlchemy #EXTENSION QUE PERMITE INTEGRAR SQLITE CON FLASK
 
-db = SQLAlchemy()
+db = SQLAlchemy() #CREA UNA INSTANCIA GLOBAL DE SQLALCHEMY
 
+#CLASE QUE REPRESENTA LA TABLA DE USUARIOS
 class Usuario(db.Model):
-    __tablename__ = 'usuarios'
-
+    __tablename__ = 'usuarios' #NOMBRE DE LA TABLA
+    #CAMPOS DE LA TABLA
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     usuario = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     contrasena = db.Column(db.String(100), nullable=False)
-
+    #PARA DEBUGUEAR Y VER COMO SE VA A MOSTRAR EL OBJETO CUANDO DEBE IMPRIMIRSE EN CONSOLA
     def __repr__(self):
         return f'<Usuario {self.usuario}>'
